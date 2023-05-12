931f76f0aadae834dd7a4df89291c579851445e9
Konstantin Mestnikov <64804506+ritec03@users.noreply.github.com>
2023-05-12 10:44:56 -0700

    refactor and adding docs

--- trivia-questions.js
+++ trivia-questions.js
@@ -1,16 +1,58 @@
 import {questions as allQuestions} from '/questions.js';
 // // Get references to relevant elements in the HTML document
 
+
+// The key used to store the list of used questions in the local storage.
+// This key is used to retrieve and store the list of questions that have been already used.
+/* Example of the structure found at the "used-questions" local storage slot:
+[
+  "questionId1",
+  "questionId2",
+  "questionId3"
+]
+*/
 const LS_USED_QUESTIONS = "used-questions";
+
+// The key used to store the trivia scores in the local storage.
+// This key is used to retrieve and store the scores awarded for each question.
+// The scores are stored as an array of objects, where each object represents 
+// a score and includes properties like question_id, team_name, and score.
+/* Example of the structure found at the "trivia-scores" local storage slot:
+[
+  {
+    question_id: "questionId1",
+    team_name: "Team A",
+    score: 200
+  },
+  {
+    question_id: "questionId2",
+    team_name: "Team B",
+    score: 100
+  },
+  {
+    question_id: "questionId3",
+    team_name: "Team A",
+    score: 300
+  }
+]
+*/
 const LS_TRIVIA_SCORES = "trivia-scores";
-const QUESTION_TIME = 11;
+
+const QUESTION_TIME = 11; // time allocated per question
+// dict to map category levels to levels of difficulty
 const diffDict = {
   100: 1,
   200: 2,
   300: 3,
 };
 
-// Define a function to generate a random question from the given category and difficulty level
+/**
+ * Generates a question from the given category and difficulty level.
+ *
+ * @param {string} category - The category of the question.
+ * @param {number} difficulty - The difficulty level of the question.
+ * @returns {Object|null} The generated question object, or null if no more unused questions are available.
+ */
 function generateQuestion(category, difficulty) {
   // Retrieve the list of questions that have not been used yet from local storage
   const usedQuestions = JSON.parse(localStorage.getItem(LS_USED_QUESTIONS)) ||
@@ -31,7 +73,14 @@ function generateQuestion(category, difficulty) {
   return selectedQuestion;
 }
 
-// Define a function to check if there are any unused questions left for the given category and difficulty level
+/**
+ * Checks if there are any unused questions left for the given category and difficulty level.
+ *
+ * @param {string} category - The category to check.
+ * @param {number} difficulty - The difficulty level to check.
+ * @returns {boolean} Returns true if there are unused questions matching the category
+ * and difficulty, otherwise false.
+ */
 function hasUnusedQuestions(category, difficulty) {
   // Retrieve the list of questions that have not been used yet from local storage
   const usedQuestions = JSON.parse(localStorage.getItem(LS_USED_QUESTIONS)) || [];
@@ -43,6 +92,9 @@ function hasUnusedQuestions(category, difficulty) {
   return questions.length > 0;
 }
 
+/**
+ * Generates the category table dynamically based on the available categories and difficulty levels.
+ */
 function generateCategoryTable() {
   const categories = new Set(allQuestions.map(q => q.category));
   const table = document.createElement('table');
@@ -82,6 +134,13 @@ function generateCategoryTable() {
   categoryTable.appendChild(table);
 }
 
+/**
+ * Creates a table cell element for the given category and difficulty.
+ *
+ * @param {string} category - The category of the table cell.
+ * @param {number} difficulty - The difficulty level of the table cell.
+ * @returns {HTMLTableCellElement} The created table cell element.
+ */
 function createTableCell(category, difficulty) {
   const cell = document.createElement('td');
   const cellClass = `${category}-${difficulty}`.trim().replace(/\s/g, '_'); // class doesn't accept whitespaces
@@ -94,12 +153,20 @@ function createTableCell(category, difficulty) {
   return cell;
 }
 
+/**
+ * Displays the category table and handles the button actions.
+ */
 function showCategoryTable() {
   generateCategoryTable();
   document.getElementById("category-table").style.display = "block";
   registerTableButtons();
 }
 
+/**
+ * Registers click event listeners for the category table buttons.
+ * Each registered event listener generates and displays a question
+ * based on the selected category and difficulty.
+ */
 function registerTableButtons() {
   const buttons = document.querySelectorAll("td button");
   buttons.forEach((button) => {
@@ -116,10 +183,13 @@ function registerTableButtons() {
   });
 }
 
-function displayQuestion(question) {
-  // Show the question and hide the table
-  const questionDiv = document.getElementById("question");
-  questionDiv.innerHTML = `<div class="question-frame">
+/**
+ * Generates the markup for displaying the question.
+ * @param {object} question - The question object.
+ * @returns {string} The generated markup for the question.
+ */
+function generateQuestionMarkup(question) {
+  const markup = `<div class="question-frame">
   <h1>The Category is: ${question.category}</h1>
   <div class="question-content">
       <h2>${question.question}</h2>
@@ -145,31 +215,25 @@ function displayQuestion(question) {
   </div>
   </div>`;
 
-  if (question.choices) {
-    addChoicesListeners(question);
-  }
+  return markup;
+}
 
-
-  document.getElementById("category-table").style.display = "none";
-  questionDiv.style.display = "block";
-
-  const correctButton = document.querySelector("#correct-button");
-  const goBackButton = document.querySelector("#go-back-button");
-  const teamSelectionDiv = document.querySelector(".team-selection");
-  const teamSelect = document.querySelector("#team-select");
-  const teamOkButton = document.querySelector("#team-ok-button");
-
+/**
+ * Starts the timer with the given time limit.
+ * @param {number} timeLimit - The time limit for the timer.
+ */
+function startTimer(timeLimit) {
   // Display the timer
-  const timeLimit = QUESTION_TIME;
   const timerDiv = document.getElementById("timer");
   timerDiv.style.display = "block";
-  timerDiv.innerHTML = `<h2>Time remaining: <span id="time-remaining">${timeLimit}</span></h2>`;
+  let timerColor = "white";
+  timerDiv.innerHTML = `<h2>Time remaining: <span style="color: ${timerColor};" id="time-remaining">${timeLimit}</span></h2>`;
 
   // Start the timer
   let timeRemaining = timeLimit;
   let intervalId = setInterval(() => {
     timeRemaining--;
-    let timerColor = "white";
+    timerColor = "white";
     if (timeRemaining <= 10 && timeRemaining > 5) {
       timerColor = "red";
     } else if (timeRemaining <= 5) {
@@ -182,8 +246,30 @@ function displayQuestion(question) {
       timerDiv.innerHTML = "<h2>Time is UP!</h2>";
     }
   }, 1000);
+}
 
-  
+/**
+ * Displays the question and handles user interactions for answering the question.
+ * @param {Object} question - The question object to display.
+ */
+function displayQuestion(question) {
+  // Show the question and hide the table
+  const questionDiv = document.getElementById("question");
+  questionDiv.innerHTML = generateQuestionMarkup(question);
+
+  if (question.choices) {
+    addChoicesListeners(question);
+  }
+
+  document.getElementById("category-table").style.display = "none";
+  questionDiv.style.display = "block";
+  startTimer(QUESTION_TIME);
+
+  const correctButton = document.querySelector("#correct-button");
+  const goBackButton = document.querySelector("#go-back-button");
+  const teamSelect = document.querySelector("#team-select");
+  const teamOkButton = document.querySelector("#team-ok-button");
+
   // Add event listeners
   if (!question.choices) {
     correctButton.addEventListener("click", function (event) {
@@ -200,74 +286,119 @@ function displayQuestion(question) {
   // this will add the score
   teamOkButton.addEventListener("click", function (event) {
     if (teamSelect.value !== "None") {
-      let teamName = teamSelect.value;
-      let scores = JSON.parse(
-        localStorage.getItem(LS_TRIVIA_SCORES) || "[]",
-      );
-      let score = {
-        question_id: question.id,
-        team_name: teamName,
-        score: question.difficulty * 100, // Replace with the actual score awarded for a correct answer
-      };
-      scores.push(score);
-      localStorage.setItem(LS_TRIVIA_SCORES, JSON.stringify(scores));
+      addScoreToLocalStorage(question, teamSelect.value);
     }
-
     // Add the question id to the used questions list and store it in local storage
-    const usedQuestions =
-      JSON.parse(localStorage.getItem(LS_USED_QUESTIONS)) || [];
-    usedQuestions.push(question.id);
-    localStorage.setItem(LS_USED_QUESTIONS, JSON.stringify(usedQuestions));
-
-    teamSelectionDiv.style.display = "none";
-    questionDiv.style.display = "none";
+    updateUsedQuestionsList(question);
+    hideQuestion();
     // Show the answer and hide the question
-    const answerDiv = document.getElementById("answer");
-      answerDiv.innerHTML = `
-      <div class="answer-frame">
-        <h1>The Answer is: ...</h1>
-        <div class="answer-content">
-          <p>${question.answer}!!</p>
-          ${question.answerImage ? `<img src="${question.answerImage}"/>` : ''}
-          <button class="continue-button">Continue</button>
-        </div>
-      </div>`;
-      answerDiv.style.display = "block";
-
-  const continueButton = document.querySelector(".continue-button");
-    continueButton.addEventListener("click", function (event) {
-      answerDiv.style.display = "none";
-        showCategoryTable();
-    })
-
+    showAnswerDiv(question);
   });
 }
 
+/**
+ * Adds the score for the question and team name to the local storage.
+ * @param {object} question - The question object.
+ * @param {string} teamName - The name of the team.
+ */
+function addScoreToLocalStorage(question, teamName) {
+  let scores = JSON.parse(localStorage.getItem(LS_TRIVIA_SCORES) || "[]");
+  let score = {
+    question_id: question.id,
+    team_name: teamName,
+    score: question.difficulty * 100,
+  };
+  scores.push(score);
+  localStorage.setItem(LS_TRIVIA_SCORES, JSON.stringify(scores));
+}
+
+/**
+ * Updates the used questions list in the local storage with the given question ID.
+ * @param {object} question - The question object.
+ */
+function updateUsedQuestionsList(question) {
+  const usedQuestions = JSON.parse(localStorage.getItem(LS_USED_QUESTIONS) || "[]");
+  usedQuestions.push(question.id);
+  localStorage.setItem(LS_USED_QUESTIONS, JSON.stringify(usedQuestions));
+}
+
+/**
+ * Hides the question and team selection div.
+ */
+function hideQuestion() {
+  const teamSelectionDiv = document.querySelector(".team-selection");
+  const questionDiv = document.getElementById("question");
+  teamSelectionDiv.style.display = "none";
+  questionDiv.style.display = "none";
+}
+
+/**
+ * Displays the answer div with the answer for the given question.
+ * @param {object} question - The question object.
+ */
+function showAnswerDiv(question) {
+  const answerDiv = document.getElementById("answer");
+  answerDiv.innerHTML = `
+    <div class="answer-frame">
+      <h1>The Answer is: ...</h1>
+      <div class="answer-content">
+        <p>${question.answer}!!</p>
+        ${question.answerImage ? `<img src="${question.answerImage}"/>` : ''}
+        <button class="continue-button">Continue</button>
+      </div>
+    </div>`;
+  answerDiv.style.display = "block";
+
+  const continueButton = document.querySelector(".continue-button");
+  continueButton.addEventListener("click", function (event) {
+    answerDiv.style.display = "none";
+    showCategoryTable();
+  });
+}
+
+/**
+ * Displays the question scoring section.
+ */
 function showQuestionScoring() {
-  const teamSelect = document.querySelector("#team-select");
   const teamSelectionDiv = document.querySelector(".team-selection");
   teamSelectionDiv.style.display = "block";
     // Populate the team select dropdown with team names from local storage
     let teamNames = getTeamNames();
-    for (const teamName of teamNames) {
-      if (
-        !Array.from(teamSelect.options).some((option) =>
-          option.value === teamName
-        )
-      ) {
-        let option = document.createElement("option");
-        option.value = teamName;
-        option.text = teamName;
-        teamSelect.add(option);
-      }
-    }
-    // create the None option
-    let noneOption = document.createElement("option");
-    noneOption.value = "None";
-    noneOption.text = "None";
-    teamSelect.add(noneOption);
+    populateTeamSelect(teamNames);
 }
 
+/**
+ * Populates the team select dropdown with team names from the given array.
+ * Also adds a "None" option to the team select dropdown that is used
+ * when no team receives the score for the question.
+ */
+function populateTeamSelect(teamNames) {
+  const teamSelect = document.querySelector("#team-select");
+  for (const teamName of teamNames) {
+    if (
+      !Array.from(teamSelect.options).some((option) =>
+        option.value === teamName
+      )
+    ) {
+      let option = document.createElement("option");
+      option.value = teamName;
+      option.text = teamName;
+      teamSelect.add(option);
+    }
+  }
+  // create the None option
+  let noneOption = document.createElement("option");
+  noneOption.value = "None";
+  noneOption.text = "None";
+  teamSelect.add(noneOption);
+}
+
+/**
+ * Adds click event listeners to the choice buttons for handling
+ * user selection and displaying question scoring.
+ *
+ * @param {Object} question - The question object.
+ */
 function addChoicesListeners(question) {
   const choiceButtons = document.querySelectorAll(".choice-item");
   choiceButtons.forEach((button) => {
@@ -285,6 +416,11 @@ function addChoicesListeners(question) {
   })
 }
 
+/**
+ * Retrieves the team names from the trivia scores in local storage.
+ *
+ * @returns {string[]} An array of team names.
+ */
 function getTeamNames() {
   let scores = JSON.parse(
     localStorage.getItem("trivia-scores") || "{}",
